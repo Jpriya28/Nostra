49fcacb16d15551d8716d3371d223e8165ff96b3
Jpriya28 <jeyap344@gmail.com>
2025-01-07 15:03:04 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,108 @@
+const products = [
+    {
+        id:1,
+        name:"Printed kurti",
+        src:"products/c1.jpg",
+        desc:"",
+        price:500,
+        tags:["old","black","kurtis"]
+    },
+
+    {
+        id:2,
+        name:"Floral kurti",
+        src:"products/c2.jpg",
+        desc:"",
+        price:550,
+        tags:["old","pink","kurtis"]
+    },
+
+    {
+        id:3,
+        name:"Design kurti",
+        src:"products/c3.jpg",
+        desc:"",
+        price:450,
+        tags:["new","black","kurtis"]
+
+    },
+
+    {
+        id:4,
+        name:"Plain kurti",
+        src:"products/c4.jpg",
+        desc:"",
+        price:500,
+        tags:["new","white","kurtis"]
+    },
+    {
+        id:5,
+        name:"Elegant saree",
+        src:"products/c5.jpg",
+        desc:"",
+        price:1000,
+        tags:["new","orange","saree"]
+    },
+
+    {
+        id:6,
+        name:"Traditional saree",
+        src:"products/c6.jpg",
+        desc:"",
+        price:1500,
+        tags:["old","red","saree"]
+    },
+
+    {
+        id:7,
+        name:"Polyestar saree",
+        src:"products/c7.jpg",
+        desc:"",
+        price:1689,
+        tags:["old","red","saree"]
+    },
+    {
+        id:8,
+        name:"Elegant saree",
+        src:"products/c8.jpg",
+        desc:"",
+        price:1500,
+        tags:["new","red","saree"]
+    },
+    {
+        id:9,
+        name:"White Tshirt",
+        src:"products/c9.jpg",
+        desc:"",
+        price:750,
+        tags:["old","white","jeans"]
+    },
+    {
+        id:10,
+        name:"Black Tshirt",
+        src:"products/c10.jpg",
+        desc:"",
+        price:640,
+        tags:["new","black","jeans"]
+    },
+    {
+        id:11,
+        name:"Black Tshirt",
+        src:"products/c11.jpg",
+        desc:"",
+        price:800,
+        tags:["old","black","jeans"]
+    },
+    {
+        id:12,
+        name:"White Tshirt",
+        src:"products/c12.jpg",
+        desc:"",
+        price:795,
+        tags:["new","whire","jeans"]
+    },
+
+
+]
+
+export {products}
